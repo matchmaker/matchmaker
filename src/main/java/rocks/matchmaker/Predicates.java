2779b870bc1bb18da854e6e63ee57275185c4931
Artur Gajowy <artur.gajowy@gmail.com>
2017-01-30 19:05:26 +0100

    Rename `Predicate.assuming` to `assumingType`

--- src/main/java/rocks/matchmaker/Predicates.java
+++ src/main/java/rocks/matchmaker/Predicates.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public interface Predicates {
 
-    static <T> Predicate<Object> assuming(Class<T> targetClass, Predicate<T> predicate) {
+    static <T> Predicate<Object> assumingType(Class<T> targetClass, Predicate<T> predicate) {
         return (x) -> (targetClass.isInstance(x)) && predicate.test(targetClass.cast(x));
     }
 }
