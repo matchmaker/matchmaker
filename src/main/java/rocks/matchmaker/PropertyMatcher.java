62b806e55dc3ebb9aec8da0b8599fe1a812151f0
Artur Gajowy <artur.gajowy@gmail.com>
2017-02-17 01:06:08 +0100

    Cosmetics in `PropertyMatcher`

--- src/main/java/rocks/matchmaker/PropertyMatcher.java
+++ src/main/java/rocks/matchmaker/PropertyMatcher.java
@@ -7,15 +7,20 @@ public class PropertyMatcher<F, R> {
     private final Function<F, Option<?>> property;
     private final Matcher<R> matcher;
 
-    private PropertyMatcher(Function<F, Option<?>> property, Matcher<R> matcher) {
-        this.property = property;
-        this.matcher = matcher;
-    }
-
     //this reflects the fact that PropertyMatcher<F, R> is contravariant on F and covaraint on R
     @SuppressWarnings("unchecked cast")
     public static <F, R> PropertyMatcher<F, R> upcast(PropertyMatcher<? super F, ? extends R> matcher) {
         return (PropertyMatcher<F, R>) matcher;
+    }
+
+    public static <F, T, R> PropertyMatcher<F, R> of(Function<F, Option<T>> property, Matcher<R> matcher) {
+        //without the ::apply below, the type system is unable to drop the R type from Option
+        return new PropertyMatcher<>(property::apply, matcher);
+    }
+
+    private PropertyMatcher(Function<F, Option<?>> property, Matcher<R> matcher) {
+        this.property = property;
+        this.matcher = matcher;
     }
 
     public Function<F, Option<?>> getProperty() {
@@ -24,10 +29,5 @@ public class PropertyMatcher<F, R> {
 
     public Matcher<R> getMatcher() {
         return matcher;
-    }
-
-    public static <F, T, R> PropertyMatcher<F, R> of(Function<F, Option<T>> property, Matcher<R> matcher) {
-        //without the ::apply below, the type system is unable to drop the R type from Option
-        return new PropertyMatcher<>(property::apply, matcher);
     }
 }
