16040fba4a830c4fb0e38b0022c38041dd304bfb
Artur Gajowy <artur.gajowy@gmail.com>
2017-07-18 00:25:50 +0200

    Add Capture#sequenceNumber

--- src/main/java/rocks/matchmaker/Capture.java
+++ src/main/java/rocks/matchmaker/Capture.java
@@ -1,8 +1,22 @@
 package rocks.matchmaker;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Capture<T> {
+
+    private static final AtomicInteger sequenceCounter = new AtomicInteger();
+
+    private final int sequenceNumber;
 
     public static <T> Capture<T> newCapture() {
         return new Capture<>();
+    }
+
+    private Capture() {
+        this.sequenceNumber = sequenceCounter.incrementAndGet();
+    }
+
+    public int sequenceNumber() {
+        return sequenceNumber;
     }
 }
